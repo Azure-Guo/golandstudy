d5b4137410b5081e73fed1d4e2ad83b9090d5430
“Youxin” <2405540664@qq.com>
2022-07-22 21:39:35 +0800

    socketServer

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
+	"time"
 )
 
 type User struct {
@@ -12,8 +12,40 @@ type User struct {
 	D bool
 	E struct{}
 }
+type myMutex chan struct{}
 
+func NewMutex() myMutex {
+	ch := make(myMutex, 1)
+	return ch
+}
+func (m *myMutex) Lock() bool {
+	timer := time.NewTimer(1 * time.Second)
+
+	select {
+	case *m <- struct{}{}:
+		return true
+	case <-timer.C:
+		fmt.Println("Time out...")
+		return false
+	}
+}
+func (m *myMutex) UnLock() {
+	select {
+	case <-*m:
+	}
+}
+func add(a *int, m *myMutex) {
+	m.Lock()
+	*a++
+	m.UnLock()
+}
 func main() {
-	fmt.Println(unsafe.Sizeof(User{}))
-	fmt.Println(unsafe.Alignof(User{}))
+	//mutex := NewMutex()
+	//j := 0
+	//for i := 0; i < 50; i++ {
+	//	go add(&j, &mutex)
+	//}
+	//time.Sleep(time.Second)
+	//fmt.Println(j)
+	DoServer("8888")
 }
